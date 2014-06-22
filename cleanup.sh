fb622998d7e51894c36745db5df1447d1c7bccd3
Jo <sdanaipat@gmail.com>
2014-06-22 16:54:03 +0200

    + check if the desktop is already empty

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,14 @@
 #!/bin/zsh
 
-echo "Cleaning up your Desktop mess!"
+if [ "$(ls ~/Desktop/)" ]; then
+    DATE=`date +%B-%d-%Y`
+    DEST=~/Desktop\ mess/${DATE}/
 
-DATE=`date +%B-%d-%Y`
-DEST=~/Desktop\ mess/${DATE}/
-
-if [ ! -d "$DEST" ]; then
-  mkdir "$DEST"
+    if [ ! -d "$DEST" ]; then
+      mkdir "$DEST"
+    fi
+    mv -f ~/Desktop/* $DEST
+    echo "All your mess has been moved to" ${DEST}.
+else
+    echo "Your desktop was already empty."
 fi
-mv -f ~/Desktop/* $DEST
-echo "All your mess has been moved to" ${DEST}.
