4d543a6347803554ab24863765dee980ff3fa68a
Jo <sdanaipat@gmail.com>
2014-06-22 17:05:19 +0200

    handle duplicate files

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,15 @@
 #!/bin/zsh
 
+MESS_PILE=~/Desktop\ mess/  # put your desired directory here
+
 if [ "$(ls ~/Desktop/)" ]; then
     DATE=`date +%B-%d-%Y`
-    DEST=~/Desktop\ mess/${DATE}/
+    DEST=${MESS_PILE}${DATE}/
 
     if [ ! -d "$DEST" ]; then
       mkdir "$DEST"
     fi
-    mv -f ~/Desktop/* $DEST
+    mv -v -i  ~/Desktop/* $DEST
     echo "All your mess has been moved to" ${DEST}.
 else
     echo "Your desktop was already empty."
